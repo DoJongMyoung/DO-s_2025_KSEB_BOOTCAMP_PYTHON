dfd1f81d0640e87c2d675f45439988dd499f6a8b
Jong Myoung <djm342@naver.com>
2025-02-11 12:31:49 +0900

    v0.52 prime

--- day02.py
+++ day02.py
@@ -1,10 +1,13 @@
 #prime number check program
+#import math
+
 n = int(input("Input number : "))
+#print(int(15**0.5))
 
 is_prime = True
-
+i = 2
 if n >= 2:
-    for i in range (2 , n):
+    for i in range (2,int(n**0.5)+1): #소수가 아니면 짝을 이루므로 제곱근과 비교해도 괜찮음.
         if n % i == 0 :
             is_prime = False
             break
