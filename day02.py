b2f884761604948b4ca2928bef7a20711bf7aad8
Jong Myoung <djm342@naver.com>
2025-02-11 17:18:37 +0900

    v0.7 prime number print

--- day02.py
+++ day02.py
@@ -1,19 +1,31 @@
-def is_prime(num):
-    i = 2
+def is_prime(num) -> bool:
     if num >= 2:
-        while i * i <= num :
+        i = 2
+        while i < (int(num ** 0.5) + 1):
             if num % i == 0:
                 return False
             i = i + 1
-
     else:
         return False
-
     return True
 
-n = int(input("Input number : "))
+# main
+#help(abs)
+#help(is_prime)
+numbers = input("Input number : ").split()  # 900 1000
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-if is_prime(n):
-    print(f"{n} is prime number")
-else:
-    print(f"{n} is NOT prime number!")
+# if n1 > n2:
+#     temp = n1
+#     n1 = n2
+#     n2 = temp
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+j = n1
+while j <= n2:
+    if is_prime(j):
+        print(j, end=' ')
+    j = j + 1
