78dfae2c30356318ee859cef852d42e0e564cbd8
Jong Myoung <djm342@naver.com>
2025-02-11 16:27:00 +0900

    v0.6 forë to while

--- day02.py
+++ day02.py
@@ -1,26 +1,19 @@
-def is_prime(num) -> bool:
-    """
-    A function that returns True if it is a prime number and False if it is not a prime number
-    :param num: integer number
-    :return: boolean type
-    """
+def is_prime(num):
+    i = 2
     if num >= 2:
-        for i in range(2, int(num ** 0.5) + 1):
+        while i * i <= num :
             if num % i == 0:
                 return False
-                #is_prime = False
-                #break
-            #print(i, end=' ')
+            i = i + 1
+
     else:
         return False
+
     return True
 
-# main
-#help(abs)
-help(is_prime)
 n = int(input("Input number : "))
 
-if is_prime(n):  # function call
+if is_prime(n):
     print(f"{n} is prime number")
 else:
     print(f"{n} is NOT prime number!")
