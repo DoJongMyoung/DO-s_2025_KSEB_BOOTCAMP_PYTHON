07af4776e4bdf35237986a70c9067e0fb0eea0ad
Jong Myoung <djm342@naver.com>
2025-02-12 19:59:40 +0900

    v2.0 menu added

--- day03.py
+++ day03.py
@@ -65,7 +65,12 @@
 #     elif number == 8 :
 #         print(f'다음에 또 오세요')
 #         break
+#while 안쪽의 하드 코딩된 코드를 함수화 하시오
 import random
+
+def print_menu(n):
+    print(f'{drinks[n]}에 어울리는 안주는 {foods[n]} 입니다')
+
 
 drinks = ["위스키", "와인", "소주", "고량주" ]
 foods = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
@@ -76,11 +81,11 @@ foods = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
 
 #del drinks_foods["위스키"]
 #drinks_foods["사케"] = "광어회"
-# drinks.append("사케")
-# drinks.append("위스키")
-#
-# foods.append("광어회")
-# foods.append("낙곱새")
+drinks.append("사케")
+drinks.append("위스키")
+
+foods.append("광어회")
+foods.append("낙곱새")
 #drink = input(drinks_foods.keys())
 #drinks_foods_keys = list(drinks_foods)
 # print(drinks_foods_keys)
@@ -96,28 +101,16 @@ for i in range(len(drinks)):
 menu_list = menu_list + f'{len(drinks) +1}) 아무거나 {len(drinks) + 2}) 종료 : '
 
 
-
-
 while True:
-    # menu = input(f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}  6) {drinks[5]}   7) 아무거나   8) 종료 : ')
+    menu = int(input(menu_list))
 
-    menu = input(menu_list)
+    if  1 <= menu <= len(drinks) :
+        print_menu(menu - 1)
 
-    if menu == '1':
-        print(f'{drinks[0]}에 어울리는 안주는 {foods[0]} 입니다')
-    elif menu == '2':
-        print(f'{drinks[1]}에 어울리는 안주는 {foods[1]} 입니다')
-    elif menu == '3':
-        print(f'{drinks[2]}에 어울리는 안주는 {foods[2]} 입니다')
-    elif menu == '4':
-        print(f'{drinks[3]}에 어울리는 안주는 {foods[3]} 입니다')
-    elif menu == '5':
-        print(f'{drinks[4]}에 어울리는 안주는 {foods[4]} 입니다')
-    elif menu == '6':
-        print(f'{drinks[5]}에 어울리는 안주는 {foods[5]} 입니다')
-    elif menu == '7':
-        random_index = random.randint(0,5)
+    elif menu == len(drinks) + 1 :
+        random_index = random.randint(0, len(drinks) - 1 )
         print(f'{drinks[random_index]}에 어울리는 안주는 {foods[random_index]} 입니다')
-    elif menu == '8':
+
+    elif menu == len(drinks) + 2 :
         print(f'다음에 또 오세요')
         break
