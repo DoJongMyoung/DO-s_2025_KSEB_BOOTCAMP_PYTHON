457f7ee888a0543b2aeb3e60eefdb677d941e413
Jong Myoung <djm342@naver.com>
2025-02-12 11:36:43 +0900

    v1.7 day03 power function

--- day02.py
+++ day02.py
@@ -1,21 +1,43 @@
-    def my_pow(b,e) -> float:
+# Assignment Day 02
+# v1.4) Make my_pow custom function instead of ** operator, power function and make it work.
+import math
+
+
+def my_pow(b, e) -> float:
     """
-A user-defined function that receives a base and exponent and returns the power result in the form of a real number    :param b:
+    A user-defined function that receives a base and exponent and returns the power result in the form of a real number
     :param b: base number
-    :param e: exponnent
+    :param e: exponent
     :return: the power result in the form of a real number
     """
+    if e < 0:
+        b = 1 / b
+        e = e * -1
+
     result = 1
-    for k in range(e):
+
+    i = int(e)
+    f = e - i
+
+    for _ in range(i):  # for k in range(e):
         result = result * b
+
+    if f > 0:
+        result = result * math.exp(f * math.log(b))
+
     return result
 
 
 def is_prime(num) -> bool:
+    """
+    A function that returns True if it is a prime number and False if it is not a prime number
+    :param num: integer number
+    :return: boolean type
+    """
     if num >= 2:
         i = 2
-       # while i < (int(my_pow(num, 0.5)) + 1):
-        while i * i < num+1 :
+        while i < (int(my_pow(num, 0.5)) + 1):
+        #while i*i < num+1:
             if num % i == 0:
                 return False
             i = i + 1
@@ -23,6 +45,8 @@ def is_prime(num) -> bool:
         return False
     return True
 
+
+#print(my_pow(2, 9))
 numbers = input("Input number : ").split()  # ex) 900 1000
 n1 = int(numbers[0])
 n2 = int(numbers[1])
