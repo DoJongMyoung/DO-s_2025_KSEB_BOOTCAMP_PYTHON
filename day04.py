39478d0ffe985d87159a44aaaa269a823b94c9df
Jong Myoung <djm342@naver.com>
2025-02-13 16:53:05 +0900

    v3.8_kwargs_added

--- day04.py
+++ day04.py
@@ -1,42 +1,25 @@
-#SOLID
-#open closed principle : 개방 폐쇄 원칙 (확장에는 열려있고 수정에는 닫혀있는 원칙)
+#v3.8)  kwargs를 이용한 데코레이터 예제
+from xmlrpc.client import WRAPPERS
 
-#v3.6) 2중 데코레이터 적용, 성능측정 데코레이터, 디스크립션 데코레이터를 팩토리얼 함수에 적용하시오.
-import time
 
-def time_decorator(func):
-    def wrapper(*arg):
-        s = time.time()
-        r = func(*arg)
-        e = time.time()
-        print(f"실행시간 : {e - s}초")
-        return r
+def log_decorator(func): #decorator는 함수를 인자로 받고, OCP원칙에 따라 안에 함수가 wrapped 상태
+    def wrapper(*args, **kWargs):
+        print(f'Function Name : {func.__name__}')
+        print(f'Function Arguments : {args}')
+        print(f'Function Keyword Arguments : {kWargs}') # **kwargs는 딕셔너리 형태
+        result = func(*args,**kWargs)
+        return result
     return wrapper
 
-def des_decorator(func):  # closure
-    def wrapper(*args):
-        print(f"함수 이름 : {func.__name__}") #매직 키워드 : __기호있는 녀석들
-        print(f"함수 설명 : {func.__doc__}") #실행되는 함수의 이름(__name__)과 설명(__doc__)을 출력함
-        r = func(*args)
-        return r
 
-    return wrapper #함수 호출이 아닌 주소를 전달하는 return
+@log_decorator
+def greet(name, greeting="안녕하세요", age=0): #default인자 greeting은 따로 인자를 주지 않으면 안녕하세요가 출력, 아닌경우는 입력한 녀석 출력
+    return f"{greeting}, {name}"
 
 
-
-# @time_decorator
-# @des_decorator
-def factorial_repetition(n) -> int:
-    """factorial function by loop"""
-    result = 1
-    for i in range(2, n+1):
-        result = result * i
-    return result
-
-number = int(input())
-t = des_decorator(time_decorator(factorial_repetition)) # 이름만 넣으면 주소를 호출 . 즉 time_decorator에 인수로 factorial_repetiton의 주소를 넣어줌.
-print(f"{number}! = {t(number)}")
-
-# number = int(input())
-# print(f"{number}! = {factorial_repetition(number)}")
+print(greet("인하"))
+print(greet("종명","안녕"))
+print(greet("James","Hello"))
+print(greet("Gonzales",greeting = "Hola"))
+print(greet("Nakamura",greeting = "Gonizizwa",age=29))
 
